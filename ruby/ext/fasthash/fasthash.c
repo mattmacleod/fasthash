be70557d2b869fca748f60f3bcd0c53309b8e9cd
Matthew MacLeod <matt@umm.io>
2016-05-27 02:48:43 +0100

    Implement postgres thingy

--- ruby/ext/fasthash/fasthash.c
+++ ruby/ext/fasthash/fasthash.c
@@ -1,33 +1,44 @@
 #include<stdio.h>
 #include<ruby.h>
 
-extern int rust_parse(void *, size_t);
+struct Result {
+  size_t len;
+  int32_t *data;
+};
+
+extern struct Result* rust_fetch(int);
 
 VALUE Fasthash = Qnil;
 VALUE Fasthash_Hash = Qnil;
 
-VALUE rb_fasthash_hash_len(VALUE self) {
-    VALUE json = rb_iv_get(self, "@json");
-    
-    int result = rust_parse(StringValuePtr(json), RSTRING_LEN(json));
+VALUE rb_fasthash_hash_fetch(VALUE self, VALUE id) {    
+    // int result = rust_parse(StringValuePtr(json), RSTRING_LEN(json));
+  FIXNUM_P(id);
+  struct Result *result = rust_fetch(NUM2INT(id));
+  int len = result->len;
+  VALUE values[len];
+  for(int i = 0; i < len; i++) {
+    values[i] = INT2NUM(result->data[i]);
+  }
+  return rb_ary_new4(len, values);
 
-    return INT2FIX(result);
 }
 
-VALUE rb_fasthash_hash_initialize(VALUE self, VALUE json)
-{
-    Check_Type(json, T_STRING);
+// TODO: create and store a Rust postgres connection
+// VALUE rb_fasthash_hash_initialize(VALUE self, VALUE json)
+// {
+//     Check_Type(json, T_STRING);
 
-    rb_iv_set(self, "@json", json);
+//     rb_iv_set(self, "@json", json);
 
-    return self;
-}
+//     return self;
+// }
 
 void Init_fasthash(void) {
     Fasthash = rb_define_module("Fasthash");
     Fasthash_Hash = rb_define_class_under(Fasthash, "Hash", rb_cObject);
 
-    rb_define_method(Fasthash_Hash, "initialize", rb_fasthash_hash_initialize, 1);
-    rb_define_method(Fasthash_Hash, "len", rb_fasthash_hash_len, 0);
+    // rb_define_method(Fasthash_Hash, "initialize", rb_fasthash_hash_initialize, 0);
+    rb_define_method(Fasthash_Hash, "fetch", rb_fasthash_hash_fetch, 1);
 }
 
